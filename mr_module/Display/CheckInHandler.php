1f799f056f8f67661599fad9b660bec133f02af0
mosen <mosen1@gmail.com>
2017-02-14 22:21:42 +1100

    Stub CheckIn Handler for Displays Info

---
+++ mr_module/Display/CheckInHandler.php
@@ -0,0 +1,30 @@
+<?php
+namespace MrModule\Display;
+
+
+use Mr\Contracts\CheckIn\Handler;
+
+class CheckInHandler implements Handler
+{
+
+    /**
+     * Determine whether MunkiReport data with the given module name may be handled by this CheckInHandler.
+     *
+     * @param $moduleName string The short name of the class of data that needs to be handled.
+     * @return boolean
+     */
+    public function canHandle($moduleName)
+    {
+        return $moduleName == 'displays_info';
+    }
+
+    /**
+     * @param $moduleName string The short name of the class of data that needs to be handled.
+     * @param $data array A hash of data to process.
+     * @return mixed
+     */
+    public function process($moduleName, $serialNumber, $data)
+    {
+        // TODO: Implement process() method.
+    }
+}
--- mr_module/Display/Display.php
+++ mr_module/Display/Display.php
@@ -7,4 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class Display extends Model
 {
     protected $table = 'display';
+
+    protected $fillable = [
+        'type',
+        'display_serial',
+        'vendor',
+        'model',
+        'manufactured',
+        'native'
+    ];
 }
