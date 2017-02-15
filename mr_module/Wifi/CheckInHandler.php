72957928b7b3f250e7224c38cb7c43f53b2e18d7
mosen <mosen1@gmail.com>
2017-02-15 21:59:18 +1100

    Add basic implementation of Wifi Checkin handler

---
+++ mr_module/Wifi/CheckInHandler.php
@@ -0,0 +1,57 @@
+<?php
+namespace MrModule\Wifi;
+
+use Mr\CheckIn\ParsesText;
+use Mr\Contracts\CheckIn\Handler;
+
+class CheckInHandler implements Handler
+{
+    use ParsesText;
+
+    /**
+     * @var array Hash of text fields in the submitted output respective to their database column names.
+     */
+    protected $translate = [
+        'agrCtlRSSI' => 'agrctlrssi',
+        'agrExtRSSI' => 'agrextrssi',
+        'agrCtlNoise' => 'agrctlnoise',
+        'agrExtNoise' => 'agrextnoise',
+        'state' => 'state',
+        'op mode' => 'op_mode',
+        'lastTxRate' => 'lasttxrate',
+        'maxRate' => 'maxrate',
+        'lastAssocStatus' => 'lastassocstatus',
+        '802.11 auth' => 'x802_11_auth',
+        'link auth' => 'link_auth',
+        'BSSID' => 'bssid',
+        'SSID' => 'ssid',
+        'MCS' => 'mcs',
+        'channel' => 'channel'
+    ];
+
+    /**
+     * Determine whether MunkiReport data with the given module name may be handled by this CheckInHandler.
+     *
+     * @param $moduleName string The short name of the class of data that needs to be handled.
+     * @return boolean
+     */
+    public function canHandle($moduleName)
+    {
+        return $moduleName == 'wifi';
+    }
+
+    /**
+     * @param $moduleName string The short name of the class of data that needs to be handled.
+     * @param $data array A hash of data to process.
+     * @return mixed
+     */
+    public function process($moduleName, $serialNumber, $data)
+    {
+        $wifi = Wifi::firstOrNew(['serial_number' => $serialNumber]);
+        $wifi->serial_number = $serialNumber;
+
+        $attrs = $this->parseTextRecord($data, ": ", $this->translate);
+        $wifi->fill($attrs);
+        $wifi->save();
+    }
+}
--- mr_module/Wifi/Wifi.php
+++ mr_module/Wifi/Wifi.php
@@ -6,5 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class Wifi extends Model
 {
+    protected $table = 'wifi';
 
+    protected $fillable = [
+        'agrctlrssi',
+        'agrextrssi',
+        'agrctlnoise',
+        'agrextnoise',
+        'state',
+        'op_mode',
+        'lasttxrate',
+        'lastassocstatus',
+        'maxrate',
+        'x802_11_auth',
+        'link_auth',
+        'bssid',
+        'ssid',
+        'mcs',
+        'channel'
+    ];
 }
