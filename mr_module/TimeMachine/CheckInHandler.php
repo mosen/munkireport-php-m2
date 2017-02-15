46df3f99f7bc1b9a082d893f7a6b030eee19c9ba
Mosen <mosen1@gmail.com>
2017-02-15 17:42:48 +1100

    Add scripts from time machine module. Started time machine checkin handler. Add required properties to timemachine model.

---
+++ mr_module/TimeMachine/CheckInHandler.php
@@ -0,0 +1,56 @@
+<?php
+namespace MrModule\TimeMachine;
+
+
+use Mr\Contracts\CheckIn\Handler;
+
+class CheckInHandler implements Handler
+{
+    /**
+     * Determine whether MunkiReport data with the given module name may be handled by this CheckInHandler.
+     *
+     * @param $moduleName string The short name of the class of data that needs to be handled.
+     * @return boolean
+     */
+    public function canHandle($moduleName)
+    {
+        return $moduleName == 'timemachine';
+    }
+
+    /**
+     * @param $moduleName string The short name of the class of data that needs to be handled.
+     * @param $serialNumber
+     * @param $data array A hash of data to process.
+     * @return mixed
+     */
+    public function process($moduleName, $serialNumber, $data)
+    {
+        $tm = TimeMachine::firstOrNew(['serial_number' => $serialNumber]);
+
+        // Parse log data
+        $start = ''; // Start date
+        foreach (explode("\n", $data) as $line) {
+            $date = substr($line, 0, 19);
+            $message = substr($line, 21);
+            
+            if (preg_match('/^Starting (automatic|manual) backup/', $message)) {
+                $start = $date;
+            } elseif (preg_match('/^Backup completed successfully/', $message)) {
+                if ($start) {
+                    $tm->duration = strtotime($date) - strtotime($start);
+                } else {
+                    $tm->duration = 0;
+                }
+                $tm->last_success = $date;
+            } elseif (preg_match('/^Backup failed/', $message)) {
+                $tm->last_failure = $date;
+                $tm->last_failure_msg = $message;
+            }
+        }
+
+        // Only store if there is data
+        if ($tm->last_success or $tm->last_failure) {
+            $tm->save();
+        }
+    }
+}
--- mr_module/TimeMachine/TimeMachine.php
+++ mr_module/TimeMachine/TimeMachine.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class TimeMachine extends Model
 {
+    protected $table = 'timemachine';
 
+    protected $fillable = [
+        'last_success',
+        'last_failure',
+        'last_failure_msg',
+        'duration'
+    ];
+
+    protected $casts = [
+        'last_success' => 'datetime',
+        'last_failure' => 'datetime'
+    ];
 }
