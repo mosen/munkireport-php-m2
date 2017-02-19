9985f498a6638125c96c12244ecfc77a183ac564
mosen <mosen1@gmail.com>
2017-02-19 18:59:08 +1100

    Hardware model widget completed, no backend api

--- resources/assets/js/components/widgets/HardwareModelWidget.vue
+++ resources/assets/js/components/widgets/HardwareModelWidget.vue
@@ -1,17 +1,19 @@
 <template>
-    <div class="col-lg-4 col-md-6">
-        <div class="panel panel-default" id="hardware-model-widget">
-            <div class="panel-heading">
-                <h3 class="panel-title"><i class="fa fa-laptop"></i> {{ $t('machine.hardware_widget_title') }}</h3>
-            </div>
-            <div class="list-group scroll-box">
-                <template v-if="data.length > 0">
-                    <a v-for="item in data" href="'/show/listing/hardware/#'+d.machine_desc" class="list-group-item">
-                        {{ machine_desc }} <span class="badge pull-right">{{ count }}</span>
-                    </a>
-                </template>
-                <span v-else class="list-group-item">{{ $t('no_clients') }}</span>
-            </div>
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            <h3 class="panel-title">
+                <i class="glyphicon glyphicon-list"></i>
+                {{ $t('machine.hardware_widget_title') }}
+            </h3>
+        </div>
+        <div class="list-group scroll-box">
+            <template v-if="data.length > 0">
+                <a v-for="item in items" href="/show/listing/hardware/machine_desc" class="list-group-item">
+                    {{ item.machine_desc }}
+                    <span class="badge pull-right">{{ count }}</span>
+                </a>
+            </template>
+            <span v-else class="list-group-item">{{ $t('no_clients') }}</span>
         </div>
     </div>
 </template>
@@ -20,13 +22,27 @@
     export default {
         data () {
             return {
+                items: [],
 
+                error: false,
+                errorDetails: {
+                    status: 200,
+                    message: ''
+                }
             }
         },
 
         beforeCreate () {
-            this.axios.get('/ext/machine').then((response) => {
-                console.log(response.json());
+            this.axios.get(`/xapi/stats/machine/hardware_models`).then((response) => {
+                if (response.data) {
+                    this.items = response.data;
+                }
+            }).catch((response) => {
+                this.error = true;
+                this.errorDetails = {
+                    status: response.status,
+                    message: response.message
+                };
             })
         }
     }
