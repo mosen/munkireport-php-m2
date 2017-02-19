9e42bde770c71c5d460865e1c39cd30253519ce5
mosen <mosen1@gmail.com>
2017-02-19 20:19:03 +1100

    Unfinished HardwareTypeWidget and InstalledMemoryWidget

---
+++ resources/assets/js/components/widgets/HardwareTypeWidget.vue
@@ -0,0 +1,24 @@
+<template>
+    <panel>
+        <i slot="title" class="fa fa-desktop"></i>
+        <span slot="title">{{ $t('machine.hardware_type_title') }}</span>
+
+        <svg style="width:100%"></svg>
+    </panel>
+</template>
+
+<script>
+    // TODO: d3 graph unfinished
+    
+    import Panel from '../bootstrap/Panel';
+    export default {
+        data() {
+            return {
+
+            }
+        },
+        components: {
+            'panel': Panel
+        }
+    }
+</script>
