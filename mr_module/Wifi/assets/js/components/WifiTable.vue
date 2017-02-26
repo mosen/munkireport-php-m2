6eb12cafe7bf2830bd0db8a5e8e6706e8bc3528f
mosen <mosen1@gmail.com>
2017-02-26 22:01:00 +1100

    Add Wifi Listing Move dataTables locale files into resources/assets/locale Created vue-table.json locale file with existing strings.

---
+++ mr_module/Wifi/assets/js/components/WifiTable.vue
@@ -0,0 +1,65 @@
+<template>
+    <v-server-table
+            url="/xapi/wifi"
+            :columns="columns"
+            :options="options"
+            @loaded="onLoaded"
+    ></v-server-table>
+</template>
+
+<script>
+    import locales from './WifiTable.i18n.json';
+
+    export default {
+        data() {
+            return {
+                columns: [
+                    'machine.computer_name',
+                    'reportdata.serial_number',
+                    'ssid',
+                    'bssid',
+                    'state',
+                    'lasttxrate',
+                    'maxrate',
+                    'channel',
+                    'agrctlrssi',
+                    'agrctlnoise',
+                    'x802_11_auth',
+                    'link_auth',
+                    'op_mode',
+                    'mcs'
+                ],
+                options: {
+                    texts: {
+                        loading: this.$t('listing.loading')
+                    },
+                    headings: {
+                        'machine.computer_name': this.$t('listing.computername'),
+                        'serial_number': this.$t('serial'),
+                        'ssid': 'SSID',
+                        'bssid': 'BSSID',
+                        'state': this.$t('listing.wifi.state'),
+                        'lasttxrate': this.$t('listing.wifi.lasttx'),
+                        'maxrate': this.$t('listing.wifi.maxtx'),
+                        'channel': this.$t('listing.wifi.channel'),
+                        'agrctlrssi': 'RSSI',
+                        'agrctlnoise': this.$t('listing.wifi.noise'),
+                        'x802_11_auth': this.$t('listing.wifi.authtype'),
+                        'link_auth': this.$t('listing.wifi.linkauthtype'),
+                        'op_mode': this.$t('listing.wifi.apmode'),
+                        'mcs': 'MCS',
+                    },
+                    pagination: {
+                        dropdown: true
+                    }
+                }
+            }
+        },
+        locales: locales,
+        methods: {
+            onLoaded: function () {
+
+            }
+        }
+    }
+</script>
