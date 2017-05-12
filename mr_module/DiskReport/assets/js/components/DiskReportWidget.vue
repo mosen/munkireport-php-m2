99c9cbd8ec3690c6cc40c98a10ad34812d587d22
Mosen <mosen1@gmail.com>
2017-05-12 17:11:17 +1000

    add panel component to disk report and fv status widgets

--- mr_module/DiskReport/assets/js/components/DiskReportWidget.vue
+++ mr_module/DiskReport/assets/js/components/DiskReportWidget.vue
@@ -1,66 +1,76 @@
 <template>
-    <div class="panel panel-default">
-        <div class="panel-heading">
-            <h3 class="panel-title">
-                <i class="glyphicon glyphicon-hdd"></i>
-                <span>{{ $t('free_disk_space') }}</span>
-            </h3>
-        </div>
-        <div class="panel-body text-center">
-            <a v-if="danger > 0" class="btn btn-danger">
-                <span class="disk-count bigger-150">{{ danger }}</span>
-                <br>
-                <span class="disk-label">&lt; {{ dangerThreshold }}</span>
-            </a>
-            <a v-if="warning > 0" class="btn btn-warning">
-                <span class="disk-count bigger-150">{{ warning }}</span>
-                <br>
-                <span class="disk-label">&lt; {{ warningThreshold }}</span>
-            </a>
-            <a v-if="success > 0" class="btn btn-success">
-                <span class="disk-count bigger-150">{{ success }}</span>
-                <br>
-                <span class="disk-label">{{ warningThreshold }} +</span>
-            </a>
+    <panel>
+        <i slot="title" class="glyphicon glyphicon-hdd"></i>
+        <span slot="title">{{ $t('free_disk_space') }}</span>
+
+        <div v-if="total === 0 && !error" class="panel-body text-center">
             <span v-if="total === 0 && !error" id="disk-nodata">{{ $t('no_clients') }}</span>
         </div>
-    </div>
+        <div v-else class="widget-status">
+            <div class="widget-status-item widget-danger widget-padded">
+                <a>
+                    <span class="disk-count bigger-150">{{ danger || 0 }}</span>
+                    <br>
+                    <span class="disk-label">&lt; {{ dangerThreshold }}</span>
+                </a>
+            </div>
+            <div class="widget-status-item widget-warning widget-padded">
+                <a>
+                    <span class="disk-count bigger-150">{{ warning || 0 }}</span>
+                    <br>
+                    <span class="disk-label">&lt; {{ warningThreshold }}</span>
+                </a>
+            </div>
+            <div class="widget-status-item widget-success widget-padded">
+                <a>
+                    <span class="disk-count bigger-150">{{ success || 0 }}</span>
+                    <br>
+                    <span class="disk-label">{{ warningThreshold }} +</span>
+                </a>
+            </div>
+        </div>
+    </panel>
 </template>
 
 <script>
-    export default {
-        data() {
-            return {
-                danger: 0,
-                warning: 0,
-                success: 0,
-                warningThreshold: 10,
-                dangerThreshold: 5,
-                error: false,
-                errorDetails: {
-                    status: 200,
-                    message: ''
-                }
-            }
-        },
-        computed: {
-            total: function () {
-                return this.danger + this.warning + this.success;
-            }
-        },
-        mounted () {
-            this.axios.get(`/xapi/stats/diskreport/free_space`).then((response) => {
-                this.danger = response.data.danger;
-                this.warning = response.data.warning;
-                this.success = response.data.success;
+  import panel from 'CoreDashboard/components/WidgetPanel.vue';
 
-                this.warningThreshold = response.data.warningThreshold;
-                this.dangerThreshold = response.data.dangerThreshold;
-            }).catch((response) => {
-                this.error = true;
-                this.errorDetails.status = response.status;
-                this.errorDetails.message = response.message;
-            })
+  export default {
+    data() {
+      return {
+        danger: 0,
+        warning: 0,
+        success: 0,
+        warningThreshold: 10,
+        dangerThreshold: 5,
+        error: false,
+        errorDetails: {
+          status: 200,
+          message: ''
         }
+      }
+    },
+    computed: {
+      total: function () {
+        return this.danger + this.warning + this.success;
+      }
+    },
+    mounted () {
+      this.axios.get(`/xapi/stats/diskreport/free_space`).then((response) => {
+        this.danger = response.data.danger;
+        this.warning = response.data.warning;
+        this.success = response.data.success;
+
+        this.warningThreshold = response.data.warningThreshold;
+        this.dangerThreshold = response.data.dangerThreshold;
+      }).catch((response) => {
+        this.error = true;
+        this.errorDetails.status = response.status;
+        this.errorDetails.message = response.message;
+      })
+    },
+    components: {
+      panel
     }
+  }
 </script>
--- mr_module/DiskReport/assets/js/components/FileVaultStatusWidget.vue
+++ mr_module/DiskReport/assets/js/components/FileVaultStatusWidget.vue
@@ -1,11 +1,8 @@
 <template>
-    <div class="panel panel-default">
-        <div class="panel-heading">
-            <h3 class="panel-title">
-                <i class="glyphicon glyphicon-lock"></i>
-                <span>{{ $t('filevault.widget.title') }}</span>
-            </h3>
-        </div>
+    <panel>
+        <i slot="title" class="glyphicon glyphicon-lock"></i>
+        <span slot="title">{{ $t('filevault.widget.title') }}</span>
+
         <div class="panel-body text-center">
             <a v-if="unencrypted > 0" class="btn btn-danger">
                 <span class="disk-count bigger-150">{{ unencrypted }}</span>
@@ -19,37 +16,42 @@
             </a>
             <span v-if="total === 0 && !error">{{ $t('no_clients') }}</span>
         </div>
-    </div>
+    </panel>
 </template>
 
 <script>
-    export default {
-        data() {
-            return {
-                encrypted: 0,
-                unencrypted: 0,
+  import panel from 'CoreDashboard/components/WidgetPanel.vue';
 
-                error: false,
-                errorDetails: {
-                    status: 200,
-                    message: ''
-                }
-            }
-        },
-        computed: {
-            total: function() {
-                return this.encrypted + this.unencrypted;
-            }
-        },
-        mounted() {
-            this.axios.get(`/xapi/stats/diskreport/filevault_status`).then((response) => {
-                this.encrypted = response.data.encrypted;
-                this.unencrypted = response.data.unencrypted;
-            }).catch((response) => {
-                this.error = true;
-                this.errorDetails.status = response.status;
-                this.errorDetails.message = response.message;
-            })
+  export default {
+    data() {
+      return {
+        encrypted: 0,
+        unencrypted: 0,
+
+        error: false,
+        errorDetails: {
+          status: 200,
+          message: ''
         }
+      }
+    },
+    computed: {
+      total: function () {
+        return this.encrypted + this.unencrypted;
+      }
+    },
+    mounted() {
+      this.axios.get(`/xapi/stats/diskreport/filevault_status`).then((response) => {
+        this.encrypted = response.data.encrypted;
+        this.unencrypted = response.data.unencrypted;
+      }).catch((response) => {
+        this.error = true;
+        this.errorDetails.status = response.status;
+        this.errorDetails.message = response.message;
+      })
+    },
+    components: {
+      panel
     }
+  }
 </script>
