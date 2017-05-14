b728c7cea40d35b56e9f4bd0a6e2ec5ac3b14aee
mosen <mosen1@gmail.com>
2017-05-14 18:13:26 +1000

    Add a dummy metrics response for testing.

--- app/Http/Controllers/MetricController.php
+++ app/Http/Controllers/MetricController.php
@@ -7,16 +7,26 @@ use Illuminate\Http\Request;
 /**
  * The MetricController class provides a single endpoint for widgets to fetch information from the system.
  * The client sends a list of metrics that it wants to receive, and each module processes and returns metrics for itself.
- * 
+ *
  * @package Mr\Http\Controllers
  */
 class MetricController extends Controller
 {
-    public function index(Request $request) {
+    public function index(Request $request)
+    {
         $topics = $request->get('topics');
 
         return response()->json([
-            "test" => $topics
+            "core.report_data" => [
+                "total" => 0,
+                "inactive_month" => 0,
+                "inactive_three_months" => 0,
+                "inactive_week" => 0,
+                "seen_last_day" => 0,
+                "seen_last_hour" => 0,
+                "seen_last_month" => 0,
+                "seen_last_week" => 0
+            ]
         ]);
     }
 }
