d9335626d57228fc23a937986dfced53a8d16e3d
mosen <mosen1@gmail.com>
2017-05-16 22:39:42 +1000

    add a temp fix for module faker factories so that we can test with dummy data

---
+++ database/factories/ModulesFactory.php
@@ -0,0 +1,27 @@
+<?php
+// Temporarily, module factories are defined here since faker does not search inside module dirs.
+
+include_once(__DIR__ . '/../../mr_module/ARD/ARDFactory.php');
+include_once(__DIR__ . '/../../mr_module/Backup2Go/Backup2GoFactory.php');
+include_once(__DIR__ . '/../../mr_module/Bluetooth/BluetoothFactory.php');
+include_once(__DIR__ . '/../../mr_module/Certificate/CertificateFactory.php');
+include_once(__DIR__ . '/../../mr_module/CrashPlan/CrashPlanFactory.php');
+include_once(__DIR__ . '/../../mr_module/DeployStudio/DeployStudioFactory.php');
+include_once(__DIR__ . '/../../mr_module/DirectoryService/DirectoryServiceFactory.php');
+include_once(__DIR__ . '/../../mr_module/DiskReport/DiskReportFactory.php');
+include_once(__DIR__ . '/../../mr_module/Display/DisplayFactory.php');
+include_once(__DIR__ . '/../../mr_module/FindMyMac/FindMyMacFactory.php');
+include_once(__DIR__ . '/../../mr_module/GSX/GSXFactory.php');
+include_once(__DIR__ . '/../../mr_module/Inventory/InventoryItemFactory.php');
+include_once(__DIR__ . '/../../mr_module/LocalAdmin/LocalAdminFactory.php');
+include_once(__DIR__ . '/../../mr_module/Location/LocationFactory.php');
+include_once(__DIR__ . '/../../mr_module/ManagedInstalls/ManagedInstallFactory.php');
+include_once(__DIR__ . '/../../mr_module/MunkiInfo/MunkiInfoFactory.php');
+include_once(__DIR__ . '/../../mr_module/MunkiReport/MunkiReportFactory.php');
+include_once(__DIR__ . '/../../mr_module/Network/NetworkFactory.php');
+include_once(__DIR__ . '/../../mr_module/Power/PowerFactory.php');
+include_once(__DIR__ . '/../../mr_module/Printer/PrinterFactory.php');
+include_once(__DIR__ . '/../../mr_module/Profile/ProfileFactory.php');
+include_once(__DIR__ . '/../../mr_module/TimeMachine/TimeMachineFactory.php');
+include_once(__DIR__ . '/../../mr_module/Warranty/WarrantyFactory.php');
+
